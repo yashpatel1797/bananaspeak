3a7144f1692876c482a89ff3a3e1ddadf180dba3
yashpatel1797 <yes.g.patel@gmail.com>
2021-08-27 18:06:01 +0530

    complete feat

---
+++ app.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#div-output");
+var minionURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return minionURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+
+}
+function clickHandler(){
+    var inputText = txtInput.value;
+    // console.log(inputText)
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+}
+btnTranslate.addEventListener("click", clickHandler);
